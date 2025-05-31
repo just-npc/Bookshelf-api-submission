b11d695fda957070636b142d476943fc1a8f6017
riiiido <121432050+just-npc@users.noreply.github.com>
2025-06-01 00:40:26 +0700

    add query parameter feature to getAllBook handler

--- src/handlers/getAllBooks.js
+++ src/handlers/getAllBooks.js
@@ -1,16 +1,40 @@
 const books = require('../books.js');
 
-const getAllBooksHandler = () => {
-  return {
+const getAllBooksHandler = (request, h) => {
+  const { name, reading, finished } = request.query;
+
+  let result = books;
+
+  if (name) {
+    const searchName = name.toLowerCase();
+
+    result = result.filter((book) => book.name.toLowerCase().includes(searchName));
+  }
+
+  if (reading === '0') {
+    result = result.filter((book) => book.reading === false);
+  } else if (reading === '1') {
+    result = result.filter((book) => book.reading === true);
+  }
+
+  if (finished === '0') {
+    result = result.filter((book) => book.finished === false);
+  } else if (finished === '1') {
+    result = result.filter((book) => book.finished === true);
+  }
+
+  const response = h.response({
     status: 'success',
     data: {
-      books: books.map((book) => ({
+      books: result.map((book) => ({
         id: book.id,
         name: book.name,
         publisher: book.publisher,
       }))
     }
-  };
+  });
+  response.code(200);
+  return response;
 };
 
 module.exports = getAllBooksHandler;
